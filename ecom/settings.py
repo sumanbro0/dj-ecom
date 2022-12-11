2452d90f2034e6be0467911503ff8f2f244843d8
sumanbro0m <pandey.suman258@gmail.com>
2022-12-12 01:01:05 +0545

    secoand

--- ecom/settings.py
+++ ecom/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-2ji-ub7kvedkjrkc8dq)yl=h=hx4vz5=*rn8)v6!yht@5rha9=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
