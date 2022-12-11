dbe03a799b641f84712d7b42ed5464dcdb6cb39e
sumanbro0m <pandey.suman258@gmail.com>
2022-12-12 02:16:15 +0545

    activate

--- base/emails.py
+++ base/emails.py
@@ -6,5 +6,5 @@ def send_account_activation_email(email, email_token):
 
     subject = "Your account needs to be verified"
     email_from = settings.EMAIL_HOST_USER
-    message = f"Hi, click on the link to activate your account {settings.DEFAULT_DOMAIN}/{email_token}/"
+    message = f"Hi, click on the link to activate your account {settings.DEFAULT_DOMAIN}/accounts/activate/{email_token}/"
     send_mail(subject, message, email_from, [email])
