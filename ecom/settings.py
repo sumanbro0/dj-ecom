e14edb8c2191a77d10ad7e6cc94016f858f876de
sumanbro0 <pandey.suman258@gmail.com>
2022-12-16 21:08:41 +0545

    db

--- ecom/settings.py
+++ ecom/settings.py
@@ -81,18 +81,18 @@ WSGI_APPLICATION = "ecom.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "railway",
-        "USER": "postgres",
-        "PASSWORD": "OV9Ja2SAgr3JzUiuiTig",
-        "HOST": "containers-us-west-114.railway.app",
-        "PORT": "8035",
-    }
     # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "NAME": "railway",
+    #     "USER": "postgres",
+    #     "PASSWORD": "OV9Ja2SAgr3JzUiuiTig",
+    #     "HOST": "containers-us-west-114.railway.app",
+    #     "PORT": "8035",
     # }
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
